484623cb0af4e6c1e1ca09144e88d5ee8e5364cd
prasenjit <99734675+E-15EFT@users.noreply.github.com>
2022-10-24 00:58:07 +0530

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -11,4 +11,7 @@ urlpatterns = [
     path('', views.lobby),
     path('room/', views.room),
     
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    
 ]
