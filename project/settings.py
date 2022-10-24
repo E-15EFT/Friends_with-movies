43c17352bf7e1b93691b67ee0e5bc550367be31a
prasenjit <99734675+E-15EFT@users.noreply.github.com>
2022-10-24 19:09:55 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 
 from pathlib import Path
-import django_heroku
+# import django_heroku
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -132,5 +132,5 @@ MEDIA_ROOT= os.path.join(BASE_DIR,"media/")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
