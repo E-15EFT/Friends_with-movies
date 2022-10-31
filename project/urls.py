5bdb2a975b921b3499e01c81409d5f1c3e52c4cf
prasenjit <99734675+E-15EFT@users.noreply.github.com>
2022-10-31 20:39:50 +0530

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -24,8 +24,8 @@ from django.conf.urls import re_path as url
 urlpatterns = [
  path('admin/', admin.site.urls),
  path('', include('myapp.urls')),
-  url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-  url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+ re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+ re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
