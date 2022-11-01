78d696d51e4af6ed78e79a9c68fafad29c15014a
prasenjit <99734675+E-15EFT@users.noreply.github.com>
2022-11-01 21:44:15 +0530

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -19,7 +19,7 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-from django.urls import re_path as url
+from django.conf.urls import re_path as url
 
 urlpatterns = [
  path('admin/', admin.site.urls),
