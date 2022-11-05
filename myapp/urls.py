389a954590ace9117ce1ba3b658a853ffc9b5088
prasenjit <99734675+E-15EFT@users.noreply.github.com>
2022-11-05 15:59:28 +0530

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,10 +1,10 @@
 
-from django.contrib import admin
+#from django.contrib import admin
 from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
     path('home/', views.index, name='home'),
     path('about/', views.about, name='about'),
     path('get_token/', views.getToken),
